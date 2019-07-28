32f612fce598286ae0015395f6fb9352d9d527bf
leeyh <gch02310@naver.com>
2019-07-28 23:23:11 +0900

    basic syntax practice

--- app/src/test/java/com/lyh/kotlin_example/ExampleUnitTest.kt
+++ app/src/test/java/com/lyh/kotlin_example/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.lyh.kotlin_example
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.lang.Exception
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +12,28 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    @Throws(Exception::class)
+    fun test() {
+        val a = 10000
+        val b = 10000
+        val c = "c"
+        var d = "d"
+        println("a == b : ${a == b}")
+        println("c == d : ${c == d}")
+        println("c === d : ${c === d}")
+        d = "c"
+        println("d = c, d == c : ${c == d}")
+        println("d = c, d === c : ${c === d}")
+        val boxedA: Int = a
+        val anotherBoxedA: Int? = a
+        println(boxedA == anotherBoxedA)
+
+        val list = mutableListOf("a", 0, 1, "B")
+        println("list : $list")
+
+        val abc = "abc"
+        for (ch in abc) {
+            println("abc char at $ch")
+        }
     }
 }
