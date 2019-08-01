23eaed81e2fd2a629672633e43fdf1419782a659
leeyh <gch02310@naver.com>
2019-08-01 16:35:24 +0900

    basic syntax practice
    kotlin control flow

--- app/src/test/java/com/lyh/kotlin_example/KotlinMethodSample.kt
+++ app/src/test/java/com/lyh/kotlin_example/KotlinMethodSample.kt
@@ -22,13 +22,11 @@ class MethodSample {
 
     // extension function
     // fun 타입.함수명(파라미터): 리턴 타입 {}
-    private fun Int.max(a: Int): Int
-            = if (this >= a) this else a
+    private fun Int.max(a: Int): Int = if (this >= a) this else a
 
     // Integer max2 a 의 형식으로 사용할 수 있다.
     // 파라미터가 하나 일 때만.
-    private infix fun Int.max2(a: Int): Int
-        = if (this >= a) this else a
+    private infix fun Int.max2(a: Int): Int = if (this >= a) this else a
 
     class ExtensionMethodTestClass {
         fun foo() = "not extension method"
@@ -39,7 +37,7 @@ class MethodSample {
     // extension properties
     // 함수 뿐 아니라 프로퍼티도 확장 가능
     val <T>List<T>.lastIndex: Int
-        get() = if(size > 0) size - 1 else 0
+        get() = if (size > 0) size - 1 else 0
 
 
     @Test
@@ -59,6 +57,73 @@ class MethodSample {
         println("\nextension properties")
         val list = mutableListOf<Int>(1, 2, 3, 4, 5)
         println("list : $list, extension property(last index) : ${list.lastIndex}")
+    }
+
+    fun whenFunction(a: Any): String {
+        val validNum = 1..10
+        return when (a) {
+            is String -> "NAN"
+            in validNum -> "$a is in validRange"
+            !in validNum -> "$a is not in vaildRange"
+            else -> "else"
+        }
+    }
+
+    fun loopFunction(list: List<Int>) {
+        for (int in list) {
+            print("$int ")
+        }
+        println()
+    }
+
+    fun loopIndex(list: List<Int>) {
+        for (i in list.indices) {
+            println("index : $i, value : ${list[i]}")
+        }
+    }
+
+    fun loopWithIndexAndValue(list: List<Int>) {
+        for ((index, value) in list.withIndex()) {
+            println("index : $index, value : $value")
+        }
+    }
+
+    fun loopWith2Step() {
+        for (value in 0..10 step 2) {
+            print("$value ")
+        }
+    }
+
+    fun loopInMap(map: Map<Int, String>) {
+        for ((key, value) in map) {
+            println("key : $key, value : $value")
+        }
+
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun controlFlow() {
+        val a = 3
+        val b = 100
+        val c = "NAN"
+        println("\n when flow")
+        println("3 : ${whenFunction(a)}")
+        println("100 : ${whenFunction(b)}")
+        println("String : ${whenFunction(c)}")
+
+        println("\n loop flow")
+        val list = mutableListOf(1, 2, 3, 4, 5, 6)
+        println("\n loop with iterator")
+        loopFunction(list)
+        println("\n loop with index")
+        loopIndex(list)
+        println("\n loop with index and value")
+        loopWithIndexAndValue(list)
+        println("\n loop with 2 step")
+        loopWith2Step()
+        println("\n loop in map")
+        loopInMap(mutableMapOf(1 to "a", 2 to "b", 3 to "c", 4 to "d"))
     }
 
 }
