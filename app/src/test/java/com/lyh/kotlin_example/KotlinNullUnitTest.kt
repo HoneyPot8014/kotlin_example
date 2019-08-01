25c4930e86069c93a5df1b4b4e64fa8cd5f6eec9
leeyh <gch02310@naver.com>
2019-08-02 01:58:20 +0900

    basic syntax practice
    handling Null in kotlin

---
+++ app/src/test/java/com/lyh/kotlin_example/KotlinNullUnitTest.kt
@@ -0,0 +1,46 @@
+package com.lyh.kotlin_example
+
+import org.junit.Before
+import org.junit.Test
+
+class NullTest {
+
+    @Before
+    fun setUp() {
+
+    }
+
+    private fun setInfo(a: String, b:String?) {
+        println("a :$a, b : $b")
+    }
+
+    private fun getSize(a: String?) = a?.length
+
+    @Test
+    @Throws(Exception::class)
+    fun test() {
+//        첫 번째 파라미터는 널을 허용하지 않음. 컴파일 타임에서 에러 발생
+//        setInfo(null, null)
+        setInfo("ABC", null)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun test2() {
+        val temp: String? = null
+        val size = getSize(temp)
+        println(size)
+    }
+
+    data class AAA(val bbb: BBB?)
+    data class BBB(val ccc: CCC?)
+    data class CCC(val name: String?)
+
+    @Test
+    @Throws(Exception::class)
+    fun test3() {
+        val aaa: AAA? = AAA(BBB(null))
+        val name = aaa?.bbb?.ccc?.name
+        println("name : $name")
+    }
+}
