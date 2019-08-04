ee7f1ef52c037b18288a6a0d8625fe2045409080
leeyh <gch02310@naver.com>
2019-08-04 18:08:20 +0900

    basic syntax practice
    handling Null in kotlin v2

--- app/src/test/java/com/lyh/kotlin_example/KotlinNullTest.kt
+++ app/src/test/java/com/lyh/kotlin_example/KotlinNullTest.kt
@@ -10,7 +10,7 @@ class NullTest {
 
     }
 
-    private fun setInfo(a: String, b:String?) {
+    private fun setInfo(a: String, b: String?) {
         println("a :$a, b : $b")
     }
 
@@ -42,5 +42,21 @@ class NullTest {
         val aaa: AAA? = AAA(BBB(null))
         val name = aaa?.bbb?.ccc?.name
         println("name : $name")
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun test4() {
+        val temp: String? = null
+        // NPE 발생
+        // val size = temp!!.length ?: 0
+
+        val size = try {
+            temp!!.length
+        } catch (e: NullPointerException) {
+            0
+        }
+
+        val size2 = temp?.length ?: throw NullPointerException("temp is null")
     }
 }
