071336c721d4ce81acfbfdd26c6edc29bb86ac4a
leeyh <gch02310@naver.com>
2019-08-12 14:00:44 +0900

    basic syntax practice
    Kotlin sealed class practice

--- app/src/test/java/com/lyh/kotlin_example/KotlinClassSample2.kt
+++ app/src/test/java/com/lyh/kotlin_example/KotlinClassSample2.kt
@@ -93,6 +93,11 @@ class CompanionClassSample2 {
     }
 }
 
+sealed class Expr(val name: String)
+class Const(val number: Double): Expr("")
+data class Sum(val e1: Expr, val e2: Expr): Expr("")
+object NotANumber: Expr("")
+
 class Test {
 
     @Test
@@ -112,6 +117,16 @@ class Test {
         // java 의 static처럼 접근
         CompanionClassSample.isTypeZero()
 
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun eval(expr: Expr): Double {
+        return when(expr) {
+            is Const -> expr.number
+            is Sum -> eval(expr.e1) + eval(expr.e2)
+            NotANumber -> Double.NaN
+        }
     }
 
 }
